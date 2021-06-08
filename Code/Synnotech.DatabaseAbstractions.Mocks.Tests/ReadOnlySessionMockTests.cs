33f6afd66dfcc88daa2f60450448544823890003
Kenny Pflug <kenny.pflug@live.de>
2021-06-08 12:19:34 +0200

    added ReadOnlySessionMock

---
+++ Code/Synnotech.DatabaseAbstractions.Mocks.Tests/ReadOnlySessionMockTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using FluentAssertions;
+using Xunit;
+
+namespace Synnotech.DatabaseAbstractions.Mocks.Tests
+{
+    public static class ReadOnlySessionMockTests
+    {
+        [Fact]
+        public static void MustBeAbstractClass() =>
+            typeof(ReadOnlySessionMock<>).Should().BeAbstract();
+
+        [Fact]
+        public static void ThrowExceptionWHenNotDisposed()
+        {
+            var session = new SessionMock();
+
+            Action act = () => session.MustBeDisposed();
+
+            act.Should().Throw<TestException>()
+               .And.Message.Should().Be("\"SessionMock\" was not disposed");
+        }
+
+        [Fact]
+        public static void NoExceptionWhenDisposed()
+        {
+            var session = new SessionMock();
+
+            session.Dispose();
+
+            session.MustBeDisposed().Should().BeSameAs(session);
+        }
+
+        private sealed class SessionMock : ReadOnlySessionMock<SessionMock> { }
+    }
+}
---
+++ Code/Synnotech.DatabaseAbstractions.Mocks/ReadOnlySessionMock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Synnotech.DatabaseAbstractions.Mocks
+{
+    /// <summary>
+    /// Represents a base class for mocks that implement <see cref="IDisposable"/>
+    /// </summary>
+    /// <typeparam name="T">The subtype that derives from this class.</typeparam>
+    public abstract class ReadOnlySessionMock<T> : IDisposable
+        where T : ReadOnlySessionMock<T>
+    {
+        /// <summary>
+        /// Gets the number of times <see cref="Dispose" /> was called.
+        /// </summary>
+        public int DisposeCallCount { get; protected set; }
+
+        /// <summary>
+        /// Increments the <see cref="DisposeCallCount" />.
+        /// </summary>
+        public void Dispose() => DisposeCallCount++;
+
+        /// <summary>
+        /// Checks if this session was disposed (<see cref="DisposeCallCount" /> must be greater or equal to 1),
+        /// or otherwise throws a <see cref="TestException" />.
+        /// </summary>
+        public T MustBeDisposed()
+        {
+            if (DisposeCallCount < 1)
+                throw new TestException($"\"{GetType().Name}\" was not disposed");
+            return (T) this;
+        }
+    }
+}
