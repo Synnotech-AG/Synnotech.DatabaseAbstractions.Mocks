b383eb909e482682642a9a38486e258fb0500fa6
Kenny Pflug <kenny.pflug@live.de>
2021-06-08 12:04:59 +0200

    added AsyncReadOnlySessionMock

---
+++ Code/Synnotech.DatabaseAbstractions.Mocks.Tests/AsyncReadOnlySessionMockTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Synnotech.DatabaseAbstractions.Mocks.Tests
+{
+    public static class AsyncReadOnlySessionMockTests
+    {
+        [Fact]
+        public static void MustBeAbstractClass() =>
+            typeof(AsyncReadOnlySessionMock<>).Should().BeAbstract();
+
+        [Fact]
+        public static void ThrowExceptionWhenNotDisposed()
+        {
+            var session = new SessionMock();
+
+            Action act = () => session.MustBeDisposed();
+
+            act.Should().Throw<TestException>()
+               .And.Message.Should().Be("\"SessionMock\" was not disposed");
+        }
+
+        [Fact]
+        public static void NoExceptionWhenDisposed()
+        {
+            var session = new SessionMock();
+
+            session.Dispose();
+
+            session.EnsureNoExceptionIsThrown();
+        }
+
+        [Fact]
+        public static async Task NoExceptionWhenDisposedAsync()
+        {
+            var session = new SessionMock();
+
+            await session.DisposeAsync();
+
+            session.EnsureNoExceptionIsThrown();
+        }
+
+        private static void EnsureNoExceptionIsThrown(this SessionMock session) =>
+            session.MustBeDisposed().Should().BeSameAs(session);
+
+        private sealed class SessionMock : AsyncReadOnlySessionMock<SessionMock> { }
+    }
+}
---
+++ Code/Synnotech.DatabaseAbstractions.Mocks/AsyncReadOnlySessionMock.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+
+namespace Synnotech.DatabaseAbstractions.Mocks
+{
+    /// <summary>
+    /// Represents a base class for mocks that implement <see cref="IAsyncReadOnlySession" />.
+    /// </summary>
+    /// <typeparam name="T">The subtype that derives from this class.</typeparam>
+    public abstract class AsyncReadOnlySessionMock<T> : IAsyncReadOnlySession
+        where T : AsyncReadOnlySessionMock<T>
+    {
+        /// <summary>
+        /// Gets the number of times <see cref="Dispose" /> or <see cref="DisposeAsync" /> was called.
+        /// </summary>
+        public int DisposeCallCount { get; protected set; }
+
+        /// <summary>
+        /// Increments the <see cref="DisposeCallCount" />.
+        /// </summary>
+        public void Dispose() => DisposeCallCount++;
+
+        /// <summary>
+        /// Increments the <see cref="DisposeCallCount" />.
+        /// </summary>
+        public ValueTask DisposeAsync()
+        {
+            DisposeCallCount++;
+            return default;
+        }
+
+        /// <summary>
+        /// Checks if this session was disposed (<see cref="DisposeCallCount" /> must be greater or equal to 1),
+        /// or otherwise throws a <see cref="TestException" />.
+        /// </summary>
+        public T MustBeDisposed()
+        {
+            if (DisposeCallCount < 1)
+                throw new TestException($"\"{GetType().Name}\" was not disposed");
+            return (T) this;
+        }
+    }
+}
