2af404515fade8062fb2c88ce934948a236542fd
Kenny Pflug <kenny.pflug@live.de>
2021-06-10 07:46:18 +0200

    added tests for AsyncDisposableMock

---
+++ Code/Synnotech.DatabaseAbstractions.Mocks.Tests/AsyncDisposableMockTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Synnotech.DatabaseAbstractions.Mocks.Tests
+{
+    public static class AsyncDisposableMockTests
+    {
+        [Fact]
+        public static void MustBeAbstractClass() =>
+            typeof(AsyncDisposableMock<>).Should().BeAbstract();
+
+        [Fact]
+        public static void MustImplementIDisposable() =>
+            typeof(AsyncDisposableMock<>).Should().Implement<IDisposable>();
+
+        [Fact]
+        public static void MustImplementIAsyncDisposable() =>
+            typeof(AsyncDisposableMock<>).Should().Implement<IAsyncDisposable>();
+
+        [Fact]
+        public static void ThrowExceptionWhenNotDisposed()
+        {
+            var disposable = new DisposableMock();
+
+            Action act = () => disposable.MustBeDisposed();
+
+            act.Should().Throw<TestException>()
+               .And.Message.Should().Be("\"DisposableMock\" was not disposed");
+        }
+
+        [Fact]
+        public static void NoExceptionWhenDisposed()
+        {
+            var disposable = new DisposableMock();
+
+            disposable.Dispose();
+
+            disposable.EnsureNoExceptionIsThrown();
+        }
+
+        [Fact]
+        public static async Task NoExceptionWhenDisposedAsync()
+        {
+            var disposable = new DisposableMock();
+
+            await disposable.DisposeAsync();
+
+            disposable.EnsureNoExceptionIsThrown();
+        }
+
+        private static void EnsureNoExceptionIsThrown(this DisposableMock disposable) =>
+            disposable.MustBeDisposed().Should().BeSameAs(disposable);
+
+        private sealed class DisposableMock : AsyncDisposableMock<DisposableMock> { }
+    }
+}
--- Code/Synnotech.DatabaseAbstractions.Mocks.Tests/AsyncReadOnlySessionMockTests.cs
+++ Code/Synnotech.DatabaseAbstractions.Mocks.Tests/AsyncReadOnlySessionMockTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using FluentAssertions;
+﻿using FluentAssertions;
 using Xunit;
 
 namespace Synnotech.DatabaseAbstractions.Mocks.Tests
@@ -12,39 +10,11 @@ namespace Synnotech.DatabaseAbstractions.Mocks.Tests
             typeof(AsyncReadOnlySessionMock<>).Should().BeAbstract();
 
         [Fact]
-        public static void ThrowExceptionWhenNotDisposed()
-        {
-            var session = new SessionMock();
-
-            Action act = () => session.MustBeDisposed();
-
-            act.Should().Throw<TestException>()
-               .And.Message.Should().Be("\"SessionMock\" was not disposed");
-        }
+        public static void MustDeriveFromAsyncDisposableMock() =>
+            typeof(AsyncReadOnlySessionMock<>).Should().BeDerivedFrom(typeof(AsyncDisposableMock<>));
 
         [Fact]
-        public static void NoExceptionWhenDisposed()
-        {
-            var session = new SessionMock();
-
-            session.Dispose();
-
-            session.EnsureNoExceptionIsThrown();
-        }
-
-        [Fact]
-        public static async Task NoExceptionWhenDisposedAsync()
-        {
-            var session = new SessionMock();
-
-            await session.DisposeAsync();
-
-            session.EnsureNoExceptionIsThrown();
-        }
-
-        private static void EnsureNoExceptionIsThrown(this SessionMock session) =>
-            session.MustBeDisposed().Should().BeSameAs(session);
-
-        private sealed class SessionMock : AsyncReadOnlySessionMock<SessionMock> { }
+        public static void MustImplementIAsyncReadOnlySession() =>
+            typeof(AsyncReadOnlySessionMock<>).Should().Implement<IAsyncReadOnlySession>();
     }
 }
